b447d8a275f9e66ae672773826adf66ac48de3d2
Nicole Gilbert <nicolegilbert16@gmail.com>
2018-01-31 08:36:04 -0800

    created react app and added files for the index and form

---
+++ searching-tree-traversal/form.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {connect} from 'react-redux';
+
+export class Form extends React.Component {
+  handleInputChange(event) {
+    const target = event.target;
+  }
+
+  render() {
+
+}
---
+++ searching-tree-traversal/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import form from './form'
+
+ReactDOM.render(
+  <Provider>
+    <Form />
+  </Provider>,
+document.getElementById('root'));
